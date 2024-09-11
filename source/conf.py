bfe48b96a95e1e6daa193772e005c88de8e77095
tsully02 <tmsully2020@gmail.com>
2024-09-11 11:39:01 -0400

    functionality for collapsible sections

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@ import pathlib
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import locs
 
